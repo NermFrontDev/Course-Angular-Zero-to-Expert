6828e96be6d884b881ec1f15c5d16263a449f1c2
NRamirez <nerm.animator@gmail.com>
2024-04-28 17:37:12 -0600

    Manual counter

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title:string = 'Curso-Angular';
   public counter: number = 10;
+
+  increaseBy( value:number ):void {
+    this.counter += value;
+  }
+
+  resetCounter():void {
+    this.counter = 10;
+  }
 }
