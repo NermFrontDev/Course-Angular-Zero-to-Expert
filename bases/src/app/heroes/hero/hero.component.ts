2c662fb4f5611b38dda7da4998ca8bdda980c782
NRamirez <nerm.animator@gmail.com>
2024-04-28 18:30:56 -0600

    One way data binding

--- bases/src/app/heroes/hero/hero.component.ts
+++ bases/src/app/heroes/hero/hero.component.ts
@@ -9,4 +9,14 @@ export class HeroComponent {
 
   public name: string = 'ironman';
   public age: number = 45;
+
+
+  public get capitalizedName() : string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${ this.name } - ${ this.age }`;
+  }
+
 }
