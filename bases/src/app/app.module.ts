2a9cbafe3c74c814ed4deb469f170f02a8ae9491
NRamirez <nerm.animator@gmail.com>
2024-04-28 22:10:42 -0600

    Modules in Angular

--- bases/src/app/app.module.ts
+++ bases/src/app/app.module.ts
@@ -3,22 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { CounterModule } from './counter/counter.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
     HeroComponent,
     ListComponent
   ],
   imports: [
     BrowserModule,
+    CounterModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// Siempre los Module irán en los imports
+// Si tiene la palabra Module, va en los imports
