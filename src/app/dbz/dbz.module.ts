490173d6d500c797b3f38b920b720138c59c8378
NRamirez <nerm.animator@gmail.com>
2024-04-29 18:09:58 -0600

    FormsModule and ngModel

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
@@ -12,6 +13,7 @@ import { AddCharacterComponent } from './components/add-character/add-character.
     AddCharacterComponent
   ],
   imports: [
+    FormsModule,
     CommonModule
   ],
   exports: [
