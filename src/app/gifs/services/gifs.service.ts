21ca76cb46e04215f0d38883eb44000d631529e3
NRamirez <nerm.animator@gmail.com>
2024-05-01 18:04:42 -0600

    Service validations

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,12 +9,26 @@ export class GifsService {
 
   constructor() { }
 
+  private organizeHistory ( tag: string ) {
+    tag = tag.toLowerCase();
+
+    if ( this._tagsHistory.includes(tag) ) {
+      // Si recibe un tag del mismo, se elimina
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag != tag );
+    }
+    // Se inserta el utlimo
+    this._tagsHistory.unshift( tag );
+    // Se delimita que muestre solo 10
+    this._tagsHistory = this.tagsHistory.splice( 0, 10 );
+  }
+
   get tagsHistory() {
     return [ ...this._tagsHistory ];
   }
 
   public searchTag ( tag:string ): void {
-    this._tagsHistory.unshift( tag );
+    if ( tag.length === 0 ) return;
+    this.organizeHistory( tag );
   }
 
 }
