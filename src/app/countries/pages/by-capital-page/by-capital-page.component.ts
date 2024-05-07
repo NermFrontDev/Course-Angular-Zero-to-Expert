2b89cc808d99f34969dc089b10201aaba4073fe5
NRamirez <nerm.animator@gmail.com>
2024-05-07 07:39:01 -0600

    SearchBox functionality

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  searchByCapital( term: string ): void {
+    console.log('Desde ByCapital Page');
+    console.log({ term });
+  }
+
 }
