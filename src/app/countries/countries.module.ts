9a5f21c14fdcf1edd2de280e13410e19925bf6a7
NRamirez <nerm.animator@gmail.com>
2024-05-07 09:50:49 -0600

    CountryTable component

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -8,6 +8,7 @@ import { CountryPageComponent } from './pages/country-page/country-page.componen
 import { CountriesRoutingModule } from './countries-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { CountryTableComponent } from './components/country-table/country-table.component';
 
 
 
@@ -16,7 +17,8 @@ import { RouterModule } from '@angular/router';
     ByCapitalPageComponent,
     ByCountryPageComponent,
     ByRegionPageComponent,
-    CountryPageComponent
+    CountryPageComponent,
+    CountryTableComponent
   ],
   imports: [
     CommonModule,
